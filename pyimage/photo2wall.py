629f72270e58959268ca2335ca43a646cb9984c6
yangpengcai <yangpengcai@kedacom.com>
2020-05-18 15:01:31 +0800

    更新

--- pyimage/photo2wall.py
+++ pyimage/photo2wall.py
@@ -8,7 +8,7 @@ import os
 
 id ='zuguo'
 
-img_path ='D:/deepspace/testThink/pyimage/zuguo3.png'
+img_path ='D:/deepspace/testThink/pyimage/zuguo5.png'
 #自己找的的模板图片存储地址，需要是矢量图
 
 #这个是我获取图片存放的文件夹，根据自己情况进行设置；
@@ -16,8 +16,8 @@ folder ="F:/王茜/test"  #{}'.format(id)
 #拼接后图片的存放位置
 save_pic_path ='D:/deepspace/testThink/pyimage/photo/{}.jpg'.format(id)
 
-img =Image.open(img_path)
-w,h =img.size#获取图片大小
+img2 =Image.open(img_path)
+w,h =img2.size#获取图片大小
 new_img = Image.new('RGB',(w,h),'#FFFFFF')#创建新的图片，大小与原图片一样
 unit_size =60
 
@@ -42,7 +42,21 @@ for i in range(x_index*y_index):
     print(f'目前进度为{i}\{x_index*y_index}')
     try:
         #利用 Image.resize()来规定图片大小，其中Image.ANATILAS表示的是高质量图片；
-        test = Image.open('{}/{}'.format(folder,pic_list[i%total])).resize((unit_size,unit_size),Image.ANTIALIAS)
+        img = Image.open('{}/{}'.format(folder,pic_list[i%total]))
+        try:
+            for orientation in ExifTags.TAGS.keys() : 
+                if ExifTags.TAGS[orientation]=='Orientation' : break 
+            exif=dict(img._getexif().items())
+            if   exif[orientation] == 3 : 
+                img=img.rotate(180, expand = True)
+            elif exif[orientation] == 6 : 
+                img=img.rotate(270, expand = True)
+            elif exif[orientation] == 8 : 
+                img=img.rotate(90, expand = True)
+        except:
+            pass
+
+        test = img.resize((unit_size,unit_size),Image.ANTIALIAS)
         new_img.paste(test, (x * unit_size, y * unit_size))
         x += 1
     except IOError:
